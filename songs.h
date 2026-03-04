a0aeec8b1b1e16cd8bf8a8c9b25f0e27ffb06333
Marc Cane <marc00077@hotmail.com>
2026-03-04 20:32:11 +0100

    generateSongsInclude.py executable

--- songs.h
+++ songs.h
@@ -15,8 +15,8 @@ Song songs[] = {
   { dr_mario_4_notes,    dr_mario_4_dur,    dr_mario_4_length, "Dr Mario 4"    },
   { dr_mario_notes,    dr_mario_dur,    dr_mario_length, "Dr Mario"    },
   { dr_mario_arranged_notes,    dr_mario_arranged_dur,    dr_mario_arranged_length, "Dr Mario Arranged"    },
-//  { dr_mario_remix_notes,    dr_mario_remix_dur,    dr_mario_remix_length, "Dr Mario Remix"    },
-   { dream_land_notes,    dream_land_dur,    dream_land_length, "Dream Land"    },
-   { duck_tales_notes,    duck_tales_dur,    duck_tales_length, "Duck Tales"    },
+  // { dr_mario_remix_notes,    dr_mario_remix_dur,    dr_mario_remix_length, "Dr Mario Remix"    },
+  { dream_land_notes,    dream_land_dur,    dream_land_length, "Dream Land"    },
+  { duck_tales_notes,    duck_tales_dur,    duck_tales_length, "Duck Tales"    },
   // { eagle_eye_cherry_save_tonight_notes,    eagle_eye_cherry_save_tonight_dur,    eagle_eye_cherry_save_tonight_length, "Eagle Eye Cherry Save Tonight"    }
 };
